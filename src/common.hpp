ecf358f352887bcafa6289a5d57a1b78bfa7efd2
Mriganka Basu Roy Chowdhury <mbrc12@gmail.com>
2025-05-21 23:17:08 -0700

    basic camera

--- src/common.hpp
+++ src/common.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <iostream>
 #include <vector>
-
+#define PI 3.14159265358979323846f
 #define DBG(x) std::cerr << __FILE__ << ":" << __LINE__ << " " << x << std::endl;
 
 std::ostream &operator<<(std::ostream &os, const std::vector<float> &v);
--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,8 @@
 constexpr int WIDTH = 640;
 constexpr int HEIGHT = 480;
 constexpr const char *GAME_NAME = "GLGame";
+float HORIZONTAL_SENSITIVITY = 0.0005f;
+float VERTICAL_SENSITIVITY = 0.0005f;
 
 GLFWwindow *window;
 
@@ -41,7 +43,7 @@ int main() {
     float last_time = glfwGetTime();
 
     Engine::Mesh cube = Engine::cuboidMesh(5.f);
-    Engine::Mesh platform = Engine::cuboidMesh(40.f, 2.f, 20.f);
+    Engine::Mesh platform = Engine::cuboidMesh(100.f, 3.f, 100.f);
     Engine::Mesh sphere = Engine::sphereMesh(10.f, 50);
 
     std::uniform_real_distribution<float> uniform(0.f, 1.f);
@@ -72,7 +74,60 @@ int main() {
     Engine::Shader shader;
     shader.build();
 
-    auto perspective = glm::perspective(glm::radians(90.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
+    auto camera = glm::identity<glm::mat4>();
+
+    static float rot_y = 0.f;
+    static float rot_x = 0.f;
+    static bool move = true;
+    static bool first_motion = true;
+
+    glfwSetCursorPosCallback(window, [](GLFWwindow *window, double x, double y) {
+        static double last_x = 0.f;
+        static double last_y = 0.f;
+
+        float dx = (float)(x - last_x);
+        float dy = (float)(y - last_y);
+
+        last_x = x;
+        last_y = y;
+ 
+        ImGui::GetIO().MousePos = ImVec2(x, y);
+
+        if (!move) {
+            return;
+        }
+
+        if (first_motion) {
+            first_motion = false;
+            return;
+        }
+
+        float new_rot_x = rot_x + dy * VERTICAL_SENSITIVITY;
+        float new_rot_y = rot_y + dx * HORIZONTAL_SENSITIVITY;
+
+        if (std::abs(new_rot_x) + std::abs(new_rot_y) > PI/2) {
+            return;
+        }
+        
+
+        rot_x = new_rot_x;
+        rot_y = new_rot_y;
+
+
+    });
+
+    auto projection = [&]() {
+        static glm::vec3 camera_position = {0.f, 50.f, 70.f};
+
+        camera = glm::rotate(glm::identity<glm::mat4>(), rot_y, glm::vec3(0.f, 1.f, 0.f));
+        camera = glm::rotate(camera, rot_x, glm::vec3(1.f, 0.f, 0.f));
+
+        static auto perspective = glm::perspective(glm::radians(45.0f), (float)WIDTH/(float)HEIGHT, 0.1f, 1000.0f);
+        return perspective * glm::inverse(camera) *
+               glm::translate(glm::identity<glm::mat4>(), -camera_position);
+    };
+
+    bool last_frame = false;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
@@ -89,24 +144,26 @@ int main() {
         glClearColor(bg_color[0], bg_color[1], bg_color[2], bg_color[3]);
 
         ImGui::Text("fps: %.2f", fps);
+        ImGui::SliderFloat("HORIZONTAL_SENSITIVITY", &HORIZONTAL_SENSITIVITY, 0.f, 0.001f, "%.5f");
+        ImGui::SliderFloat("VERTICAL_SENSITIVITY", &VERTICAL_SENSITIVITY, 0.f, 0.001f, "%.5f");
 
         imguiEnd();
 
         shader.use();
-    
+
         auto transform = glm::identity<glm::mat4>();
-        transform = glm::translate(transform, glm::vec3(0.0f, 0.0f, -20.f));
+        transform = glm::translate(transform, glm::vec3(0.f, 20.f, 0.f));
         transform = glm::rotate(transform, float(glfwGetTime()), glm::vec3(1.0f, 1.0f, 1.0f));
-        shader.setMat4Uniform("transform", perspective * transform);
+        shader.setMat4Uniform("transform", projection() * transform);
 
         crate_texture.bind();
         // cube.draw();
         // transform = glm::scale(transform, glm::vec3(1.f, 1.f, 1.f) * 8.f);
-        shader.setMat4Uniform("transform", perspective * transform);
+        shader.setMat4Uniform("transform", projection() * transform);
         sphere.draw();
 
-        transform = glm::translate(glm::identity<glm::mat4>(), glm::vec3(0.f, -15.f, -20.f));
-        shader.setMat4Uniform("transform", perspective * transform);
+        transform = glm::identity<glm::mat4>();
+        shader.setMat4Uniform("transform", projection() * transform);
 
         checkerboard_texture.bind();
         platform.draw();
@@ -117,6 +174,13 @@ int main() {
 
         if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
             glfwSetWindowShouldClose(window, true);
+
+        if (glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS && !last_frame) {
+            move = !move;
+            if (move == false) first_motion = true;
+        }
+        last_frame = glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS;
+
     }
 
     cleanup();
@@ -134,13 +198,19 @@ void imguiEnd() {
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
-void onResize(GLFWwindow *window, int width, int height) {
-    float scale = std::min(1.0 * width/WIDTH, 1.0 * height/HEIGHT);
+void onResize(GLFWwindow *window, int width = -1, int height = -1) {
+    if (width == -1 || height == -1) {
+        glfwGetWindowSize(window, &width, &height);
+    }
+
+    float scale = std::min(1.0 * width / WIDTH, 1.0 * height / HEIGHT);
     auto width_ = static_cast<size_t>(std::round(WIDTH * scale));
     auto height_ = static_cast<size_t>(std::round(HEIGHT * scale));
 
     auto x_ = (width - width_) / 2;
     auto y_ = (height - height_) / 2;
+
+    // DBG("scale: " << scale);
 
     glViewport(x_, y_, width_, height_);
     DBG("window resized to " << width << "x" << height);
@@ -154,12 +224,22 @@ int init() {
     if (!glfwInit())
         return -1;
 
+    auto monitor = glfwGetPrimaryMonitor();
+
+    const auto mode = glfwGetVideoMode(monitor);
+    glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+    glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+    glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+    glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(WIDTH, HEIGHT, GAME_NAME, NULL, NULL);
+    window = glfwCreateWindow(mode->width, mode->height, GAME_NAME, monitor, NULL);
     if (!window) {
         glfwTerminate();
         return -1;
     }
+   
+    DBG("window created: " << mode->width << "x" << mode->height);
 
     glfwSetFramebufferSizeCallback(window, onResize);
 
@@ -171,19 +251,24 @@ int init() {
         return -1;
     }
 
+    onResize(window);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    assert(glfwRawMouseMotionSupported());
+    glfwSetInputMode(window, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
+
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO &io = ImGui::GetIO();
+    io.MouseDrawCursor = true;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init();
 
-    DBG("vendor: " << glGetString(GL_VENDOR) << "\nversion: " << glGetString(GL_VERSION) << 
-        "\nshader: "
+    DBG("vendor: " << glGetString(GL_VENDOR) << "\nversion: " << glGetString(GL_VERSION) << "\nshader: "
                    << glGetString(GL_SHADING_LANGUAGE_VERSION) << "\nrenderer: " << glGetString(GL_RENDERER)
                    << "\nimgui: " << IMGUI_VERSION << "\nglfw: " << glfwGetVersionString());
 
-    glViewport(0, 0, WIDTH, HEIGHT);
+
 
     // Enable blending
     glEnable(GL_BLEND);
@@ -197,7 +282,6 @@ int init() {
     return 0;
 }
 
-
 void cleanup() {
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
@@ -206,5 +290,3 @@ void cleanup() {
     glfwDestroyWindow(window);
     glfwTerminate();
 }
-
-
